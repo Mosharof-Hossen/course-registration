e9e4593c3c16644bdaa9302b637b60ca381f157d
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-13 23:03:18 +0600

    total credit hour added

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,32 @@ import Header from './Components/Header/Header'
 
 function App() {
   const [courses, setCourse] = useState([]);
+  const [remainingTime, setRemainingTime] = useState(20);
+  const [credit, setCredit] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  const handleSelectCourse = (course) =>{
-    const findCourse = courses.find((crs)=>crs.id === course.id);
-    if(findCourse){
+  const handleSelectCourse = (course) => {
+    let updateTime = credit + course.course_duration;
+    const findCourse = courses.find((crs) => crs.id === course.id);
+    if (findCourse) {
       alert("This Course Already selected.")
       return
+    } else if (updateTime > 20) {
+      alert("You have not enough Credit Hour")
+      return
     }
-    setCourse([...courses,course]);
+    setCredit(credit + course.course_duration)
+    setCourse([...courses, course]);
   }
   console.log(courses);
-
+  console.log(credit);
   return (
     <>
       <div className='container mx-auto px-8'>
         <Header></Header>
         <main className='flex flex-col-reverse md:flex-row'>
-          <Courses handleSelectCourse = {handleSelectCourse}></Courses>
-          <CartList courses={courses}></CartList>
+          <Courses handleSelectCourse={handleSelectCourse}></Courses>
+          <CartList courses={courses} credit = {credit}></CartList>
         </main>
       </div>
     </>
--- src/Components/CartList/CartList.jsx
+++ src/Components/CartList/CartList.jsx
@@ -1,22 +1,22 @@
 
 import PropTypes from 'prop-types';
 
-const CartList = ({ courses }) => {
+const CartList = ({ courses ,credit}) => {
     return (
         <div className='md:w-1/4 mb-5 md:mb-0  bg-white rounded-lg p-6'>
-            <h2 className='text-blue-700 font-bold'>Credit Hour Remaining </h2>
+            <h2 className='text-blue-700 font-bold'>Credit Hour Remaining {20-credit} hr</h2>
             <hr className='my-3' />
             <h1 className='text-xl font-bold'>Course Name</h1>
 
             <ul className='mt-3 text-gray-500'>
                 {
-                    courses.map(course => (
-                        <li className='text-sm' key={course.id}>{course.id}. {course.title}</li>
+                    courses.map((course,inx) => (
+                        <li className='text-sm' key={inx}>{course.id}. {course.title} - {course.course_duration}h</li>
                     ))
                 }
             </ul>
             <hr className='my-3' />
-            <p className='text-lg font-semibold'>Total Credit Hour: </p>
+            <p className='text-lg font-semibold'>Total Credit Hour: {credit}</p>
             <hr className='my-3' />
             <p className='text-lg font-semibold'>Total Price: </p>
         </div>
@@ -24,7 +24,8 @@ const CartList = ({ courses }) => {
 };
 
 CartList.propTypes = {
-    courses: PropTypes.array.isRequired
+    courses: PropTypes.array.isRequired,
+    credit:PropTypes.number.isRequired
 };
 
 export default CartList;
